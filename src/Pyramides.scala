53ba9095ad0ea9095d43fbf34dff26be2d4978c8
Bruno SINTEFF <bruno.sinteff@telecomnancy.eu>
2016-10-21 08:57:57 +0200

    create Pyramides

---
+++ src/Pyramides.scala
@@ -0,0 +1,55 @@
+
+
+class Pyramides {
+  
+  def indice(l,col):Int={
+    return l*(l-1)/2+col-1
+  }
+  
+  var hauteur = 3
+  var taille = hauteur*(h+1)/2
+  var permutations : List[Array[Int]] = Nil
+  
+  def duplique(src : Array[Int]) : Array[Int]{
+    var dst = Array.fill(src.length)(0)
+    for(i<-0 to src.length-1)
+      dst(i)=src(i)
+    return dst
+  }
+  
+  def genere(rang : Int,tab : Array[Int]){
+    if(rang >= tab.length){
+      permutations = duplique(tab)::permutations
+    }
+    else{
+      for(valeur <- 1 to tab.length){
+        var dejaPris = false
+        for(i<-0 to rang-1){
+          if(tab(i)==valeur)) dejaPris = true
+          if(!dejaPris){
+            tab(rang)=valeur
+            genere(rang+1,tab)
+          }
+        }
+      }
+    }
+  }
+  
+  genere(0,Array.fill(taille)(0))
+  
+  def correcte(tab:Array[Int]):Boolean = {
+    var permutation = new String()
+    for(i<-0 tob.length-1){
+      permutation += tab(i)
+    }
+    for(ligne<-1 to hauteur-1){
+      for(col<-1 to ligne){
+        val n1 = tab(indice(ligne+1,col))
+        val n2 = tab(indice(ligne+1,col+1))
+        val n3 = tab(indice(ligne,col))
+        if(math.abs(n1-n2)!n3) return false
+      }
+    }
+   return true
+  }
+}
